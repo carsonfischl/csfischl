9ef55fb1510524096fbda60577e99623a04dfcdb
carsonfischl <carson.fischl@carleton.ca>
2026-01-06 20:59:25 -0500

    returned navbar to previous state

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,41 +19,31 @@ export function Navbar() {
   const isMobile = useIsMobile()
 
   return (
-    <NavigationMenu viewport={isMobile} className="w-screen px-4 py-2 bg-white border-b dark:bg-black dark:border-b-slate-700 m-auto gap-3 flex justify-center h-content-center items-center h-40 font-mono">
+    <NavigationMenu viewport={isMobile} className="w-100-vw bg-white border-b dark:bg-black dark:border-b-slate-700 m-auto gap-3 flex justify-center h-content-center items-center h-20 font-mono">
       <NavigationMenuList className="flex-wrap">
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link className="before:block before:absolute before:-inset-3 before:-skew-y-3 before:bg-black relative inline-block m-10" href='/'>
-                <div className="relative text-white text-3xl hover:text-slate-700">Carson Fischl</div>
-            </Link>
+            <Link href="/">Home</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link className="before:circle before:absolute before:-inset-3 before:-skew-y-3 before:bg-black relative inline-block m-7" href='/about'>
-                <div className="relative text-white text-3xl hover:text-slate-700">About</div>
-            </Link>
+            <Link href="/about">About</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link className="before:circle before:absolute before:-inset-3 before:-skew-y-3 before:bg-black relative inline-block m-7" href='/education'>
-                <div className="relative text-white text-3xl hover:text-slate-700">Education</div>
-            </Link>
+            <Link href="/education">Education</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link className="before:circle before:absolute before:-inset-3 before:-skew-y-3 before:bg-black relative inline-block m-7" href='/experience'>
-                <div className="relative text-white text-3xl hover:text-slate-700">Experience</div>
-            </Link>
+            <Link href="/experience">Experience</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link className="before:circle before:absolute before:-inset-3 before:-skew-y-3 before:bg-black relative inline-block m-7" href='/projects'>
-                <div className="relative text-white text-3xl hover:text-slate-700">Projects</div>
-            </Link>
+            <Link href="/projects">Projects</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
