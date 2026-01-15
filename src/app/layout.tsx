5da510203abeea1157f5e8b35ff0f106a869fa67
carsonfischl <carson.fischl@carleton.ca>
2026-01-15 18:31:18 -0500

    added analytics component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
             </main>
           </NextIntlClientProvider>
         {/* </SidebarProvider> */}
+        <Analytics />
       </body>
     </html>
   );
